59c926315fe7dea3fa67f1a58fdba056e0655097
Martin Kikegawa <mkikegawa@gmail.com>
2015-02-14 21:39:38 -0500

    adding rspec tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,11 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pets.all 
+  end  
+
+  def show
+    @pet = Pet.find(params[:id]) 
+  end
+
+  
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  
+
+  def index 
+    @user = User.all
+  end
+ 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+
+  def create
+    @user = User.new(params)
+    if @user.save
+      render_to @user, notice: 'User was successful created.'
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Item updated.'
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'Item deleted.'
+    redirected_to user_path
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile)
+  end
+  
+end
